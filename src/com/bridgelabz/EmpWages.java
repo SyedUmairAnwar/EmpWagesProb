4efc85050cfd85811799dc792ba146e52fa091ea
syed <umaranwar707@gmail.com>
2020-10-20 19:54:35 +0530

    Added:Working hour condition and days

--- src/com/bridgelabz/EmpWages.java
+++ src/com/bridgelabz/EmpWages.java
@@ -10,19 +10,27 @@ public class EmpWages {
 	int full_Day_Hour=8;
 	int Part_Day_Hour=4;
 	int days=20;
-	int checkout=(int) (Math.floor(Math.random() *10) %3);
+	int workingDays=0;
+	int workingHour=1;
+	int totalWages=0;
+	while (workingDays <= days && workingHour <=100 ) {
+	int checkout=(int) (Math.floor(Math.random() *10) %2+1);
 	System.out.println(checkout);
 	switch(checkout){
 		case 1 :int total=Wages_Per_Hour * full_Day_Hour;
-							System.out.println(total * days);
+							workingHour=workingHour+full_Day_Hour;
+							totalWages=totalWages+total;
 							break;
 		case 2 :int total1=Wages_Per_Hour * Part_Day_Hour;
-							System.out.println(total1 * days);
+							workingHour=workingHour + Part_Day_Hour;
+							totalWages=totalWages+total1;
 							break;
 		default : System.out.println("0");
 	}
-						
+	workingDays++;					
 	}
+	System.out.println("total wages in month="+ totalWages);
 	}
+}
 
 
