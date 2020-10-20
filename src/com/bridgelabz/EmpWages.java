0f7731ccd99386930ee57e7577d70fb3a80af776
syed <umaranwar707@gmail.com>
2020-10-20 17:22:05 +0530

    Added:part time wages

--- src/com/bridgelabz/EmpWages.java
+++ src/com/bridgelabz/EmpWages.java
@@ -3,21 +3,22 @@ package com.bridgelabz;
 public class EmpWages {
 
 	public static void main(String[] args) {
-	int present=1;
+	int fullTime=1;
+	int partTime=2;
 	int Wages_Per_Hour=20;
 	int full_Day_Hour=8;
-	int checkout=(int) (Math.floor(Math.random() *10) %2);
+	int Part_Day_Hour=4;
+	int checkout=(int) (Math.floor(Math.random() *10) %3);
 	System.out.println(checkout);
-	if (checkout == present) {
+	if (checkout == fullTime) {
 		int Total_Wages=Wages_Per_Hour * full_Day_Hour;
 		System.out.println(Total_Wages);
 	}
-	else
-		System.out.println("absent");
-	
-	
-	
-
+	else if (partTime == checkout) {
+		int total=Part_Day_Hour * Wages_Per_Hour;
+		System.out.println(total);
+	}
+		
 	}
 
 }
