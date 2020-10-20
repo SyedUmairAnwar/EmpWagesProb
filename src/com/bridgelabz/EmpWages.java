99bccc3887eaf65378946f01d7f96c32e9e0fb8d
syed <umaranwar707@gmail.com>
2020-10-20 16:55:20 +0530

    Added:Employee Status

--- src/com/bridgelabz/EmpWages.java
+++ src/com/bridgelabz/EmpWages.java
@@ -3,8 +3,18 @@ package com.bridgelabz;
 public class EmpWages {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("welcome to employee wages problem");
+	int present=1;
+	int checkout=(int) (Math.floor(Math.random() *10) %2);
+	System.out.println(checkout);
+	if (checkout == present) {
+		System.out.println("present");
+	}
+	else
+		System.out.println("absent");
+	
+	
+	
+
 	}
 
 }
