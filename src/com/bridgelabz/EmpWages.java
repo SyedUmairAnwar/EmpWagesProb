a819eb502887ca046285a0415668174134026925
syed <umaranwar707@gmail.com>
2020-10-20 17:06:44 +0530

    Added:Daily Wages

--- src/com/bridgelabz/EmpWages.java
+++ src/com/bridgelabz/EmpWages.java
@@ -4,10 +4,13 @@ public class EmpWages {
 
 	public static void main(String[] args) {
 	int present=1;
+	int Wages_Per_Hour=20;
+	int full_Day_Hour=8;
 	int checkout=(int) (Math.floor(Math.random() *10) %2);
 	System.out.println(checkout);
 	if (checkout == present) {
-		System.out.println("present");
+		int Total_Wages=Wages_Per_Hour * full_Day_Hour;
+		System.out.println(Total_Wages);
 	}
 	else
 		System.out.println("absent");
