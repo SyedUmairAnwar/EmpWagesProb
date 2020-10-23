8e61397229af51272dc576cae5f6bb2b858f6544
syed <umaranwar707@gmail.com>
2020-10-23 10:06:39 +0530

    Added:Different company wages

--- src/com/bridgelabz/EmpWages.java
+++ src/com/bridgelabz/EmpWages.java
@@ -9,7 +9,21 @@ public class EmpWages {
 		ew.WagesCalculation();
 	}
 		public void WagesCalculation(){
-			int Wages_Per_Hour=20;
+			int Wages_Per_Hour=0;
+			int company=(int) (Math.floor(Math.random() *10) %3+1);
+			switch (company) {
+			case 1: Wages_Per_Hour=20;
+					System.out.println("company is Hp having wage for hour is 20");
+					break;
+			case 2: Wages_Per_Hour=40;
+					System.out.println("company is lenovo having wage for hour is 40");
+					break;
+				
+			case 3: Wages_Per_Hour=60;
+					System.out.println("company is dell having wage for hour is 60");
+					break;
+			}
+			
 			int full_Day_Hour=8;
 			int Part_Day_Hour=4;
 			int days=20;
@@ -18,13 +32,12 @@ public class EmpWages {
 			int totalWages=0;
 			while (workingDays <= days && workingHour <100 ) {
 			int checkout=(int) (Math.floor(Math.random() *10) %2+1);
-			System.out.println("the day in "+workingDays+" is "+checkout+" hours");
 			switch(checkout){
-				case  FULL_TIME:int total=Wages_Per_Hour * full_Day_Hour;
+				case  FULL_TIME:int total=  Wages_Per_Hour * full_Day_Hour;
 									workingHour=workingHour+full_Day_Hour;
 									totalWages=totalWages+total;
 									break;
-				case PART_TIME :int total1=Wages_Per_Hour * Part_Day_Hour;
+				case PART_TIME :int total1= Wages_Per_Hour * Part_Day_Hour;
 									workingHour=workingHour + Part_Day_Hour;
 									totalWages=totalWages+total1;
 									break;
