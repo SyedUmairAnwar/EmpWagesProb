07c3afb3ede1ee60f2e1afe5e9d833efcf908fcf
syed <umaranwar707@gmail.com>
2020-10-29 10:12:24 +0530

    Added:Linked List

--- src/com/bridgelabz/EmpWages.java
+++ src/com/bridgelabz/EmpWages.java
@@ -1,54 +1,62 @@
 package com.bridgelabz;
 
+import java.util.LinkedList;
 
 public class EmpWages {
-	public static final int FULL_TIME = 1;
-	public static final int PART_TIME=2;
-	public static void main(String[] args) {
-		EmpWages ew=new EmpWages();
-		ew.WagesCalculation();
-	}
-		public void WagesCalculation(){
-			int Wages_Per_Hour=0;
-			int company=(int) (Math.floor(Math.random() *10) %3+1);
-			switch (company) {
-			case 1: Wages_Per_Hour=20;
-					System.out.println("company is Hp having wage for hour is 20");
-					break;
-			case 2: Wages_Per_Hour=40;
-					System.out.println("company is lenovo having wage for hour is 40");
-					break;
-				
-			case 3: Wages_Per_Hour=60;
-					System.out.println("company is dell having wage for hour is 60");
-					break;
-			}
-			
-			int full_Day_Hour=8;
-			int Part_Day_Hour=4;
-			int days=20;
-			int workingDays=1;
-			int workingHour=0;
-			int totalWages=0;
-			while (workingDays <= days && workingHour <100 ) {
-			int checkout=(int) (Math.floor(Math.random() *10) %2+1);
-			switch(checkout){
-				case  FULL_TIME:int total=  Wages_Per_Hour * full_Day_Hour;
-									workingHour=workingHour+full_Day_Hour;
-									totalWages=totalWages+total;
-									break;
-				case PART_TIME :int total1= Wages_Per_Hour * Part_Day_Hour;
-									workingHour=workingHour + Part_Day_Hour;
-									totalWages=totalWages+total1;
-									break;
-				default : System.out.println("0");
-			}
-			workingDays++;					
-			}
-			System.out.println("total wages in month="+ totalWages);
-			System.out.println("total working hours is "+workingHour);
-		}
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
 	
+	private int numofCompany=0;
+	private LinkedList<CompanyEmpWage>companyEmpWageList;
+	
+	public EmpWages() {
+		companyEmpWageList=new LinkedList<>();
+	}
+	private void addCompanyEmpWage(String company,int empRatePerHour,int workingDays,int maxHours) {
+		CompanyEmpWage	companyEmpWage=new CompanyEmpWage(company, empRatePerHour, workingDays, maxHours);
+		companyEmpWageList.add(companyEmpWage);
+	}
+
+
+	private void computeEmpWage(){
+		for (int i=0;i<numofCompany;i++){
+			CompanyEmpWage companyEmpWage=companyEmpWageList.get(i);
+			companyEmpWage.setTotalEmpWage(this.computeEmpWages( companyEmpWage));
+			System.out.println(companyEmpWage);
+			}
+		}
+		private int computeEmpWages(CompanyEmpWage companyEmpWage) {
+				int emphrs = 0, totalworkingdays = 0, totalEmphrs = 0;
+				while (totalEmphrs < companyEmpWage.maxHours && totalworkingdays < companyEmpWage.workingDays) 
+				{
+					totalworkingdays++;
+					int random = (int) Math.floor(Math.random() * 10) % 3;
+					switch (random) {
+
+					case 1:
+						emphrs = 8;
+						break;
+
+					case 2:
+						emphrs = 4;
+						break;
+
+					default:
+						emphrs = 0;
+
+					}
+					totalEmphrs += emphrs;
+					System.out.println("days" +totalworkingdays+ "Emphrs" +emphrs);
+				}
+				return totalEmphrs+companyEmpWage.empRateperHour;
+				
+			}
+		public static void main(String[] args) {
+			EmpWages empWageBuilder = new EmpWages();
+			empWageBuilder.addCompanyEmpWage("HP",20,20,100);
+			empWageBuilder.addCompanyEmpWage("Asus",22,23,103);
+			empWageBuilder.computeEmpWage();
+		}
 }
 	 
 
